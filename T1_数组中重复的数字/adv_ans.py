f16eeca36ea1e59f5496e91aa3b270ee5e2b8c13
EricFuma <36584629+EricFuma@users.noreply.github.com>
2020-03-29 17:00:13 +0800

    Update adv_ans.py

--- T1_数组中重复的数字/adv_ans.py
+++ T1_数组中重复的数字/adv_ans.py
@@ -1,6 +1,9 @@
-# 1~n  n+1
+
+# 元素值域：1~n   数组长度：n+1
+
+
 nums = [7, 1, 4, 6, 2, 5, 3, 6]  # n = 7, 
-pos = [i for i in range(1, len(nums)+1)]
+
 #nums = [2, 3, 5, 4, 3, 2, 6, 7]
 #nums = [0] + nums   # 0占位用
 def count_num(left, mid, right):
@@ -25,5 +28,5 @@ def find_ele(left, right):
             return find_ele(mid+1, right)
     else:
         return left
-
+# 注意find_ele中的参数是值域的左右边界（闭区间），与nums中的位置无关
 print(find_ele(1, len(nums)-1))
